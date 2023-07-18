50a16f62c63101bf4495698ba328400ffd76d7be
techbase2brand <techbase2brand@gmail.com>
2023-07-18 22:42:17 +0530

    changes

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -327,7 +327,7 @@ useEffect(() => {
               </Badge>
 
               <Avatar className="avatar" icon={<UserOutlined />} />
-              <span className="username">Vikash Soni</span>
+              <span className="username">{myData?.firstName}{myData?.lastName}</span>
               <button onClick={logout}>logout</button>
             </div>
           </div>
